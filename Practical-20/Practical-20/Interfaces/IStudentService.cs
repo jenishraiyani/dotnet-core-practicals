4d297dd7ff88342a8f8f4f03765d2c2fad7c5c48
jenishraiyani <jenish.r@simformsolutions.com>
2023-06-21 18:00:18 +0530

    Practical-20

---
+++ Practical-20/Practical-20/Interfaces/IStudentService.cs
@@ -0,0 +1,10 @@
+﻿using Practical_20.Models;
+
+namespace Practical_20.Interfaces
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<Students>> GetAll();
+        Task AddStudent(Students student);
+    }
+}
---
+++ Practical-20/Practical-20/Services/StudentService.cs
@@ -0,0 +1,32 @@
+﻿using Practical_20.Interfaces;
+using Practical_20.Models;
+
+namespace Practical_20.Services
+{
+    public class StudentService : IStudentService
+    {
+        public IUnitOfWork _unitOfWork;
+        public StudentService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public async Task AddStudent(Students students)
+        {
+            var student = new Students
+            {
+                Email = students.Email,
+                StudentName = students.StudentName,
+                Phone = students.Phone,
+            };
+
+            _unitOfWork.StudentRepository.Add(students);
+            await _unitOfWork.CommitAsync();
+        }
+
+
+        public async Task<IEnumerable<Students>> GetAll()
+            => await _unitOfWork.StudentRepository.GetAll();
+    }
+}
