a1552ab0147c28c39b2fa73ef7d582267c7209e9
jenishraiyani <jenish.r@simformsolutions.com>
2023-06-21 17:58:11 +0530

    Practical-18

---
+++ Practical-18/Practical-18/Models/StudentDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practical_18.Models
+{
+    public class StudentDto
+    {
+        
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
